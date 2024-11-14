8b8ffae6386cd222429ac5b7aa2a64b7a10c5b34
Maryam-tli <m.tavakoolii1400@gmail.com>
2024-11-14 18:47:25 +0330

    Add function to get the closest match

--- translate_chat_bot.py
+++ translate_chat_bot.py
@@ -2,3 +2,14 @@
 from difflib import get_close_matches
 from textblob import TextBlob
 from googletrans import Translator
+
+
+# Function to get the closest match for the user's question from the knowledge base
+def get_best_match(user_question: str, questions: dict) -> str | None:
+    questions: list[str] = [q for q in questions]
+    matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.6)  # Match threshold is 60%
+
+    if matches:
+        return matches[0]
+
+
